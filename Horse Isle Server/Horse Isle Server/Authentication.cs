e8e0a0a519d0f3a36cad9d5cb9161d511dd588a3
SilicaAndPina <earsyum@gmail.com>
2020-09-29 14:33:36 +1300

    Inital Commit

---
+++ Horse Isle Server/Horse Isle Server/Authentication.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Horse_Isle_Server
+{
+    class Authentication
+    {
+        public static string DecryptLogin(string encpass)
+        {
+            string decrypt = "";
+            string ROTPOOL = "bl7Jgk61IZdnY mfDN5zjM2XLqTCty4WSEoKR3BFVQsaUhHOAx0rPwp9uc8iGve";
+            string POSPOOL = "DQc3uxiGsKZatMmOS5qYveN71zoPTk8yU0H2w9VjprBXWn l4FJd6IRbhgACfEL";
+            string ROTPOOL2 = "evGi8cu9pwPr0xAOHhUasQVFB3RKoESW4ytCTqLX2Mjz5NDfm YndZI16kgJ7lb";
+
+
+            int i = 0;
+            int ii = 0;
+            while (i < encpass.Length)
+            {
+                int ROT = ROTPOOL.IndexOf(encpass[i].ToString());
+                int POS = POSPOOL.IndexOf(encpass[i + 1].ToString());
+                POS -= (ROT + ii);
+                if (POS < 0)
+                {
+                    POS = (POS / -1) - 1;
+
+                    while (POS >= ROTPOOL.Length)
+                    {
+                        POS -= ROTPOOL.Length;
+                    }
+
+                    decrypt += ROTPOOL2[POS];
+                }
+                else
+                {
+                    while (POS >= ROTPOOL.Length)
+                    {
+                        POS -= ROTPOOL.Length;
+                    }
+
+                    decrypt += ROTPOOL[POS];
+                }
+
+                i += 2;
+                ii += 1;
+            }
+            return decrypt.Replace(" ", "");
+        }
+
+
+
+        public static byte[] HashAndSalt(string plaintext, byte[] salt)
+        {
+            byte[] plaintextBytes = Encoding.UTF8.GetBytes(plaintext);
+
+            SHA512 sha512 = SHA512.Create();
+            sha512.Initialize();
+            byte[] hash = sha512.TransformFinalBlock(plaintextBytes, 0x00, plaintextBytes.Length);
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                hash[i] ^= salt[i];
+            }
+
+            sha512 = SHA512.Create();
+            sha512.Initialize();
+            byte[] finalHash = sha512.TransformFinalBlock(hash, 0x00, hash.Length);
+
+            return finalHash;
+        }
+
+        public static bool CheckPassword(string username, string password)
+        {
+
+            try
+            {
+                byte[] expectedPassword = Database.GetPasswordHash(username);
+                byte[] salt = Database.GetPasswordSalt(username);
+                byte[] hashedPassword = HashAndSalt(password, salt);
+
+                if (Enumerable.SequenceEqual(expectedPassword, hashedPassword))
+                    return true;
+                else
+                    return false;
+            }
+            catch(KeyNotFoundException e)
+            {
+                Logger.DebugPrint(e.Message);
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Horse Isle Server/Horse Isle Server/Client.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Horse_Isle_Server
+{
+    class Client
+    {
+        public Socket ClientSocket;
+        public string RemoteIp;
+        public bool LoggedIn = false;
+
+        private Thread recvPackets;
+
+        private const byte PACKET_LOGIN = 0x7F;
+        private const byte PACKET_CHAT = 0x14;
+        private const byte PACKET_MOVE = 0x15;
+        private const byte PACKET_USERINFO = 0x81;
+
+        private void receivePackets()
+        {
+            // HI1 Packets are terminates by 0x00 so we have to read until we receive that terminator
+            MemoryStream ms = new MemoryStream();
+
+            while(ClientSocket.Connected)
+            {
+                try
+                {
+                    if (ClientSocket.Available >= 1)
+                    {
+                        byte[] buffer = new byte[ClientSocket.Available];
+                        ClientSocket.Receive(buffer);
+
+
+                        foreach (Byte b in buffer)
+                        {
+                            ms.WriteByte(b);
+                            if (b == 0x00)
+                            {
+                                ms.Seek(0x00, SeekOrigin.Begin);
+                                byte[] fullPacket = ms.ToArray();
+                                parsePackets(fullPacket);
+                                ms.Close();
+                                ms = new MemoryStream();
+                            }
+                        }
+
+                    }
+                }
+                catch(SocketException e)
+                {
+                    Logger.ErrorPrint("Socket exception occured: " + e.Message +" and so it was disconnected.");
+                    Disconnect();
+                    break;
+                }
+
+            }
+
+                
+        }
+
+        private void parsePackets(byte[] Packet)
+        {
+            if (Packet.Length < 1)
+            {
+                Logger.ErrorPrint("Received an invalid packet (size: "+Packet.Length+")");
+            }
+
+            byte identifier = Packet[0];
+            if(!LoggedIn) // Must be either login or policy-file-request
+            {
+                if(Encoding.UTF8.GetString(Packet).StartsWith("<policy-file-request/>")) // Policy File Request
+                {
+                    Server.OnCrossdomainPolicyRequest(this);
+                }
+                switch(identifier)
+                {
+                    case PACKET_LOGIN:
+                        Server.OnLoginRequest(this,Packet);
+                        break;
+                }
+            }    
+        }
+
+       public void Disconnect()
+        {
+            Server.ConnectedClients.Remove(this);
+            ClientSocket.Dispose();
+        }
+
+       public void SendPacket(byte[] PacketData)
+        {
+            ClientSocket.Send(PacketData);
+        }
+
+        public Client(Socket clientSocket)
+        {
+            ClientSocket = clientSocket;
+            RemoteIp = clientSocket.RemoteEndPoint.ToString();
+
+            Logger.DebugPrint("Client connected @ " + RemoteIp);
+
+            recvPackets = new Thread(() =>
+            {
+                receivePackets();
+            });
+            recvPackets.Start();
+        }
+    }
+}
