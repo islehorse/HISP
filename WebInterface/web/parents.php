9d0b99457eb4c5357da202b48deaae57fe84752a
Li <li@silica.codes>
2020-09-29 14:31:32 +1300

    Add files via upload

---
+++ WebInterface/web/parents.php
@@ -0,0 +1,241 @@
+<HEAD>
+<TITLE>HORSE ISLE - Online Multiplayer Horse Game</TITLE>
+<META NAME="keywords" CONTENT="Horse Game Online MMORPG Multiplayer Horses RPG Girls Girly Isle World Island Virtual Horseisle Sim Virtual">
+<META NAME="description" CONTENT="A multiplayer online horse world where players can capture, train, care for and compete their horses against other players. A very unique virtual sim horse game.">
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
+<link rel="meta" href="http://horseisle.com/labels.rdf" type="application/rdf+xml" title="ICRA labels" />
+<meta http-equiv="pics-Label" content='(pics-1.1 "http://www.icra.org/pics/vocabularyv03/" l gen true for "http://horseisle.com" r (n 0 s 0 v 0 l 0 oa 0 ob 0 oc 0 od 0 oe 0 of 0 og 0 oh 0 c 1)  gen true for "http://hi1.horseisle.com" r (n 0 s 0 v 0 l 0 oa 0 ob 0 oc 0 od 0 oe 0 of 0 og 0 oh 0 c 1))' />
+<style type="text/css">
+hr {
+height: 1;
+color: #000000;
+background-color: #000000;
+border: 0;
+}
+a {
+font: bold 14px arial;
+color: #6E3278;
+}
+TH {
+background-color: #EDE5B4;
+padding: 1px 6px;
+border: 2px dotted #6E3278;
+font: small-caps 900 14px arial;
+color: #000000;
+}
+TR.a0 {
+background-color: #EDE5B4;
+}
+TR.a1 {
+background-color: #D4CCA1;
+}
+TD {
+font: 14px arial;
+color: #000000;
+}
+TD.forum {
+font: 12px arial;
+color: #000000;
+}
+TD.forumlist {
+padding: 1px 6px;
+border: 2px dotted #6E3278;
+background-color: #EDE5B4;
+text-align: center;
+font: bold 14px arial;
+color: #000000;
+}
+TD.forumpost {
+padding: 5px 10px;
+border: 2px dotted #6E3278;
+background-color: #EDE5B4;
+text-align: left;
+}
+TD.adminforumpost {
+padding: 5px 20px;
+border: 2px dotted #6E3278;
+background-color: #BFE9C9;
+text-align: left;
+}
+TD.newslist {
+padding: 4px 4px;
+border: 2px dotted #6E3278;
+background-color: #FFDDEE;
+text-align: left;
+font: 14px arial;
+color: #000000;
+}
+FORUMSUBJECT {
+font: bold 14px arial;
+color: #004400;
+}
+FORUMUSER {
+font: 12px arial;
+color: #000044;
+}
+FORUMDATE {
+font: 12px arial;
+color: #444444;
+}
+FORUMTEXT {
+font: 14px arial;
+color: #440000;
+}
+
+</style>
+</HEAD>
+<BODY BGCOLOR=E0D8AA>
+<TABLE BORDER=0 CELLPADDING=0 CELLSPACING=0 WIDTH=100%>
+<TR WIDTH=100%>
+<TD WIDTH=512 ROWSPAN=3><A HREF=/><IMG SRC=/web/hoilgui1.gif ALT="Welcome to Horse Isle" BORDER=0></A></TD>
+<TD WIDTH=100% BACKGROUND=/web/hoilgui2.gif>&nbsp;</TD>
+<TD WIDTH=29><IMG SRC=/web/hoilgui3.gif></TD>
+</TR>
+<TR>
+<TD WIDTH=100% BACKGROUND=/web/hoilgui4.gif align=right>
+<B>
+
+<TABLE CELLPADDING=0 CELLSPACING=2 BORDER=0><FORM METHOD=POST ACTION=/account.php>
+<TR><TD><B>USER:</B></TD><TD><INPUT TYPE=TEXT SIZE=14 NAME=USER></TD></TR>
+<TR><TD><B>PASS:</B></TD><TD><INPUT TYPE=PASSWORD SIZE=14 NAME=PASS></TD></TR>
+<TR><TD></TD><TD><INPUT TYPE=SUBMIT VALUE=LOGIN> (<A HREF=/web/forgotpass.php>Forgot?</A>)</TD></TR></FORM></TABLE>
+
+</TD>
+<TD WIDTH=29><IMG SRC=/web/hoilgui5.gif></TD></TR>
+<TR>
+<TD WIDTH=100% BACKGROUND=/web/hoilgui6.gif>&nbsp;</TD>
+<TD WIDTH=29><IMG SRC=/web/hoilgui7.gif></TD></TR>
+</TABLE>
+<CENTER>
+<BR>
+<CENTER><TABLE CELLPADDING=5><TR><TD>
+
+<font size="4" style="COLOR:#990000"><span style="FONT-WEIGHT:bold">Horse Isle
+Parents Guide</span></font><br/>
+<br/>
+Welcome.&nbsp; We will try to make this brief.&nbsp; First off,&nbsp; thank you
+for taking the time to check on your child's activities online.&nbsp; This is
+the first, and most important step in protecting your child.&nbsp;<br/>
+<br/>
+<span style="FONT-WEIGHT:bold; COLOR:#990000">What Horse Isle Is:</span><br/>
+<br/>
+An online game that revolves around horses in a virtual world full of games and
+adventures and other real players.<br/>
+<br/>
+<span style="FONT-WEIGHT:bold; COLOR:#990000">Horse Isle's Educational
+Benefits:</span><br/>
+<br/>
+<span style="FONT-WEIGHT:bold">Horse education:</span> Game is based on many
+real breeds of horses with accurate drawings of them and detailed information.<br/>
+<br/>
+<span style="FONT-WEIGHT:bold">Educational mini games:</span> There are many
+mini-games in Horse Isle.&nbsp; The best "paying" ones, by far, are
+educational.&nbsp; There is a geography game, a horse anatomy game, math games,
+etc.<br/>
+<br/>
+<span style="FONT-WEIGHT:bold">Riddles / Brain teasers:</span> There are
+hundreds of riddles and brain teasers in the game that the player may encounter.<br/>
+<br/>
+<span style="FONT-WEIGHT:bold">Lots of Reading:</span>&nbsp; The adventures in
+the game all require quite a bit of reading.&nbsp; The player needs to interact
+with virtual "residents"&nbsp; who may ask things of the player.&nbsp; Players
+need to use lots of Reading Comprehension and Problem Solving to accomplish the
+many adventures in the game.<br/>
+<br/>
+<span style="FONT-WEIGHT:bold">Money Management:</span>&nbsp; There are many
+ways to make virtual money in the game but even many more ways to spend
+it!&nbsp; There are some very rewarding goals in the game which cost a LOT of
+money, and the only way to meet those goals is to save up smartly.&nbsp; Also,
+virtual banks provide interest on player's virtual money when it is kept there.<br/>
+<br/>
+<span style="FONT-WEIGHT:bold">Horse Care:</span> To compete well in Arenas, and
+to keep happy horses,&nbsp; players must care for their virtual horses.&nbsp;
+This involves feeding, watering, petting, grooming, shoeing and sometimes taking
+them to the vet.<br/>
+<br/>
+<span style="FONT-WEIGHT:bold">Getting along with others:</span> Since there are
+many other players playing the same game, a player learns to interact with
+others in a positive manner.<br/>
+<br/>
+<span style="FONT-WEIGHT:bold; COLOR:#990000">What steps does Horse Isle take to
+protect my child:</span><br/>
+<br/>
+<span style="FONT-WEIGHT:bold">Rules:</span> Please view our <A HREF=rules.php>RULES</A> list.&nbsp;
+Go through them with your child and make sure that your child understands each
+and every one of them.&nbsp; These rules are all enforced to the best of our
+abilities.&nbsp; Please stress to your child that someone could try to trick
+them into giving away their account password.&nbsp; Reinforce that the password
+should never be typed anywhere, except when logging in, and to never give the
+password to anyone, not even siblings.&nbsp; Horse Isle staff will NEVER ask for
+your password.<br/>
+<br/>
+<span style="FONT-WEIGHT:bold">"Abuse" system:</span>&nbsp; We have an "Abuse"
+button set up in game under PLAYERS so if you or your child sees someone breaking the rules, it can be
+reported to Horse Isle staff.&nbsp; Recent chat will be sent along with the report.<br/>
+<br style="FONT-WEIGHT:bold"/>
+<span style="FONT-WEIGHT:bold">Chat Filtering:</span>&nbsp; We try our best to
+setup smart filtering of chat texts.&nbsp; If someone swears, our filters will
+attempt to block that and warn the player.&nbsp; Each player is only allowed a certain
+number of blocked profanity.&nbsp; After that, their account may be terminated.  
+We even attempt to replace "chat speak" with its true meaning (lol=laughing out loud) 
+and replace some borderline words with more child friendly versions.<br/>
+<br/>
+<span style="FONT-WEIGHT:bold">Moderators:</span>&nbsp; These are players in the game that have been hand-picked by the game administrators.  Their role is to be sure rules are followed and players are not going around the filter or harassing each other.  The Moderators are volunteers and have the ability to mute a player's chat if they are misbehaving.<br/>
+<br/>
+<span style="FONT-WEIGHT:bold">Administrators:</span>&nbsp; These are people that work directly on the game.  Administrators have the ability to also mute player's chat if a player is misbehaving, but they can also see all chat, including private messages.  These are the people that review the abuse reports sent in, the chat that is blocked due to the filter and answer any mail that is sent into support.  Every report and every blocked chat is reviewed by hand.<br/>  
+<br/>
+<span style="FONT-WEIGHT:bold">Content:</span>&nbsp; All of the content in the
+game has been designed for ALL AGES.&nbsp; This means there is nothing in the
+game that is violent or cruel, etc.&nbsp; Our goal has been to keep everything
+rated "G".  There is no horse breeding in this game, so it avoids all of the language involved with that.<br/>
+<br/>
+<span style="FONT-WEIGHT:bold">No Advertising:</span> We have opted for a
+pay-for-play revenue model rather than an ad supported one.&nbsp; This means
+your child will NOT be subjected to any external advertising of dubious origins.<br/>
+<br/>
+<span style="FONT-WEIGHT:bold">Parental Controls:</span> We have built in
+additional options for parents with young children.&nbsp; There is an ability to
+block ALL CHAT, which limits the game's social enjoyment but keeps a child safe
+from any chatting.&nbsp; We also provide the option of limiting a child's online
+time per day.  These options are available with a subscribed account.  There is no automated way to do this yet.  Please email support via the Contact Us link if interested in these features.<br/>
+<br/>
+<span style="FONT-WEIGHT:bold; COLOR:#990000">What are the costs involved to
+play this game:</span><br/>
+<br/>
+<span style="FONT-WEIGHT:bold">Free:</span>&nbsp; Players can play this game
+completely free with a limited account.&nbsp;&nbsp; The limited account only
+allows a certain amount of playtime per day and does not allow certain advanced
+features, such as ranch ownership.<br/>
+<br/>
+<span style="FONT-WEIGHT:bold">$5mo OR $40yr USD:</span>&nbsp; Players have full
+access to all of the game's features and support the game's continued existence
+and development.&nbsp; Parental controls are also available with a paid
+account.&nbsp; Subscription payments are safely, easily, and quickly paid via
+PayPal.&nbsp;<br/>
+<BR><CENTER> <B><A HREF=/>RETURN HOME</A>
+</TD></TR></TABLE>
+<TABLE BORDER=0 CELLPADDING=0 CELLSPACING=0 WIDTH=100%>
+<TR>
+<TD><IMG SRC=/web/hoilgui10.gif></TD>
+<TD WIDTH=100% BACKGROUND=/web/hoilgui11.gif></TD>
+<TD><IMG SRC=/web/hoilgui12.gif></TD>
+</TR></TABLE>
+<CENTER><B>
+[ <A HREF=//master.horseisle.com/beginnerguide/>New Player Guide</A> ]<BR>
+[ <A HREF=/web/rules.php>Rules</A> ]
+[ <A HREF=/web/termsandconditions.php>Terms and Conditions</A> ]
+[ <A HREF=/web/privacypolicy.php>Privacy Policy</A> ]</B><BR>
+[ <A HREF=/web/expectedbehavior.php>Expected Behavior</A> ]
+[ <A HREF=/web/contactus.php>Contact Us</A> ] 
+[ <A HREF=/web/credits.php>Credits</A> ]<BR>
+<FONT FACE=Verdana,Arial SIZE=-2>Copyright &copy; 2020 Horse Isle</FONT>
+
+<!-- Google Analytics -->
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-1805076-1";
+urchinTracker();
+</script>
+
---
+++ WebInterface/web/privacypolicy.php
@@ -0,0 +1,197 @@
+<HEAD>
+<TITLE>HORSE ISLE - Online Multiplayer Horse Game</TITLE>
+<META NAME="keywords" CONTENT="Horse Game Online MMORPG Multiplayer Horses RPG Girls Girly Isle World Island Virtual Horseisle Sim Virtual">
+<META NAME="description" CONTENT="A multiplayer online horse world where players can capture, train, care for and compete their horses against other players. A very unique virtual sim horse game.">
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
+<link rel="meta" href="http://horseisle.com/labels.rdf" type="application/rdf+xml" title="ICRA labels" />
+<meta http-equiv="pics-Label" content='(pics-1.1 "http://www.icra.org/pics/vocabularyv03/" l gen true for "http://horseisle.com" r (n 0 s 0 v 0 l 0 oa 0 ob 0 oc 0 od 0 oe 0 of 0 og 0 oh 0 c 1)  gen true for "http://hi1.horseisle.com" r (n 0 s 0 v 0 l 0 oa 0 ob 0 oc 0 od 0 oe 0 of 0 og 0 oh 0 c 1))' />
+<style type="text/css">
+hr {
+height: 1;
+color: #000000;
+background-color: #000000;
+border: 0;
+}
+a {
+font: bold 14px arial;
+color: #6E3278;
+}
+TH {
+background-color: #EDE5B4;
+padding: 1px 6px;
+border: 2px dotted #6E3278;
+font: small-caps 900 14px arial;
+color: #000000;
+}
+TR.a0 {
+background-color: #EDE5B4;
+}
+TR.a1 {
+background-color: #D4CCA1;
+}
+TD {
+font: 14px arial;
+color: #000000;
+}
+TD.forum {
+font: 12px arial;
+color: #000000;
+}
+TD.forumlist {
+padding: 1px 6px;
+border: 2px dotted #6E3278;
+background-color: #EDE5B4;
+text-align: center;
+font: bold 14px arial;
+color: #000000;
+}
+TD.forumpost {
+padding: 5px 10px;
+border: 2px dotted #6E3278;
+background-color: #EDE5B4;
+text-align: left;
+}
+TD.adminforumpost {
+padding: 5px 20px;
+border: 2px dotted #6E3278;
+background-color: #BFE9C9;
+text-align: left;
+}
+TD.newslist {
+padding: 4px 4px;
+border: 2px dotted #6E3278;
+background-color: #FFDDEE;
+text-align: left;
+font: 14px arial;
+color: #000000;
+}
+FORUMSUBJECT {
+font: bold 14px arial;
+color: #004400;
+}
+FORUMUSER {
+font: 12px arial;
+color: #000044;
+}
+FORUMDATE {
+font: 12px arial;
+color: #444444;
+}
+FORUMTEXT {
+font: 14px arial;
+color: #440000;
+}
+
+</style>
+</HEAD>
+<BODY BGCOLOR=E0D8AA>
+<TABLE BORDER=0 CELLPADDING=0 CELLSPACING=0 WIDTH=100%>
+<TR WIDTH=100%>
+<TD WIDTH=512 ROWSPAN=3><A HREF=/><IMG SRC=/web/hoilgui1.gif ALT="Welcome to Horse Isle" BORDER=0></A></TD>
+<TD WIDTH=100% BACKGROUND=/web/hoilgui2.gif>&nbsp;</TD>
+<TD WIDTH=29><IMG SRC=/web/hoilgui3.gif></TD>
+</TR>
+<TR>
+<TD WIDTH=100% BACKGROUND=/web/hoilgui4.gif align=right>
+<B>
+
+<TABLE CELLPADDING=0 CELLSPACING=2 BORDER=0><FORM METHOD=POST ACTION=/account.php>
+<TR><TD><B>USER:</B></TD><TD><INPUT TYPE=TEXT SIZE=14 NAME=USER></TD></TR>
+<TR><TD><B>PASS:</B></TD><TD><INPUT TYPE=PASSWORD SIZE=14 NAME=PASS></TD></TR>
+<TR><TD></TD><TD><INPUT TYPE=SUBMIT VALUE=LOGIN> (<A HREF=/web/forgotpass.php>Forgot?</A>)</TD></TR></FORM></TABLE>
+
+</TD>
+<TD WIDTH=29><IMG SRC=/web/hoilgui5.gif></TD></TR>
+<TR>
+<TD WIDTH=100% BACKGROUND=/web/hoilgui6.gif>&nbsp;</TD>
+<TD WIDTH=29><IMG SRC=/web/hoilgui7.gif></TD></TR>
+</TABLE>
+<CENTER>
+<BR>
+<CENTER><TABLE CELLPADDING=5><TR><TD>
+
+
+<div style="TEXT-ALIGN:center">
+  <font size="4" style="COLOR:#990000"><span style="FONT-WEIGHT:bold">Horse Isle
+  Privacy Statement</span></font><br/>
+</div>
+<br/>
+<br style="FONT-WEIGHT:bold; COLOR:#990000"/>
+<span style="FONT-WEIGHT:bold"><span style="COLOR:#990000">YOUR PERSONAL
+INFORMATION:</span><br/>
+</span><span style="FONT-STYLE:italic">UNDER
+13:</span><span style="FONT-WEIGHT:bold">&nbsp; </span>If a player is 12 years
+old or younger, we will collect the Parent/Guardian's email instead of the
+player's.&nbsp; We collect no personally identifiable information from players
+under 13 years old.<br/>
+<br/>
+Horse Isle recognizes your privacy. We collect certain information to allow us
+to run the game. We store usernames and passwords along with a player's email,
+age, and country when you sign-up. We use cookies to allow secure access by
+users. Subscribers who do not accept cookies from the domain "horseisle.com"
+cannot access most areas of the site.&nbsp; We log site and game activity (such
+as the IP address of users) to allow us to better manage the site.&nbsp; We may
+use this information to exclude visitors who violate our rules.<br/>
+<br/>
+We have clear rules disallowing communication between players of personally
+identifiable information.&nbsp; We also strive to have usernames that do not
+contain any "hints" to the player's true identity.&nbsp; Emails and IP addresses
+of players are not visible to anyone who is not a Horse Isle staff member.<br/>
+<br/>
+The only personally identifiable information we require from anyone is a valid
+email address.&nbsp; The email address is only used to send the initial
+activation of the account, and to make sure only one account is setup per email.
+The email will not be used again except for password recovery by request of a
+player.&nbsp; Other information asked for is not personally identifiable.&nbsp;
+That is country of residence, gender (determines whether you are referred to as
+he/she in the game) and age .<br/>
+<br/>
+Horse Isle does not use advertising as a source of revenue.&nbsp; So NO
+information is sold to others or provided to others for marketing information.<br/>
+<br/>
+All actions within the game may be logged and reviewed by Horse Isle
+staff.&nbsp; This includes, but is not limited to, chats and private chats.<br/>
+<br/>
+Upon closing an account ALL information is deleted about a player.&nbsp;
+including the email and game profile we stored.<br/>
+<br/>
+<span style="FONT-WEIGHT:bold; COLOR:#990000">YOUR ONLINE ACTIVITY:</span><br/>
+All chats, including private chats, and descriptions (player, horse or ranch)
+may be monitored by Horse Isle staff and moderators.&nbsp; At our discretion
+this content may be reproduced for any purpose,&nbsp; or given to any authority
+such as parents, FBI, etc.<br/>
+<br/>
+If we ever discover any sign of predator behavior we will pass it along to the
+proper authorities with as much identifiable information that we may have.&nbsp;
+Including player's username, password, email, IP address, chat logs, payment
+information, etc..&nbsp; We put online safety above the privacy of an
+individual.&nbsp; NEVER make any attempt to find out where another player lives
+or any personally identifiable information.<br/>
+<br/>
+
+</TD></TR></TABLE>
+<TABLE BORDER=0 CELLPADDING=0 CELLSPACING=0 WIDTH=100%>
+<TR>
+<TD><IMG SRC=/web/hoilgui10.gif></TD>
+<TD WIDTH=100% BACKGROUND=/web/hoilgui11.gif></TD>
+<TD><IMG SRC=/web/hoilgui12.gif></TD>
+</TR></TABLE>
+<CENTER><B>
+[ <A HREF=//master.horseisle.com/beginnerguide/>New Player Guide</A> ]<BR>
+[ <A HREF=/web/rules.php>Rules</A> ]
+[ <A HREF=/web/termsandconditions.php>Terms and Conditions</A> ]
+[ <A HREF=/web/privacypolicy.php>Privacy Policy</A> ]</B><BR>
+[ <A HREF=/web/expectedbehavior.php>Expected Behavior</A> ]
+[ <A HREF=/web/contactus.php>Contact Us</A> ] 
+[ <A HREF=/web/credits.php>Credits</A> ]<BR>
+<FONT FACE=Verdana,Arial SIZE=-2>Copyright &copy; 2020 Horse Isle</FONT>
+
+<!-- Google Analytics -->
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-1805076-1";
+urchinTracker();
+</script>
+
