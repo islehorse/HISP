99100d91cb198f58c92a1dbe690a70f0f12e359a
SilicaAndPina <earsyum@gmail.com>
2020-12-21 12:09:25 +1300

    oops;.

---
+++ Horse Isle Server/Horse Isle Server/Game/GameExceptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HISP.Game
+{
+    // Inventory
+    class InventoryException : Exception { };
+    class InventoryFullException : InventoryException { };
+    class InventoryMaxStackException : InventoryException { };
+}
