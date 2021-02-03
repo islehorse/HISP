6c42c57bfead1a835e6da9dc31ee442d47b2480c
SilicaAndPina <earsyum@gmail.com>
2021-02-03 17:27:34 +1300

    Implement "Read Books"

---
+++ Horse Isle Server/HorseIsleServer/Game/Book.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HISP.Game.Services
+{
+    public class Book
+    {
+        private static List<Book> libaryBooks = new List<Book>();
+        public static Book[] LibaryBooks
+        {
+            get
+            {
+                return libaryBooks.ToArray();
+            }
+        }
+
+        public int Id;
+        public string Title;
+        public string Author;
+        public string Text;
+        public Book(int id, string title, string author, string text)
+        {
+            Id = id;
+            Title = title;
+            Author = author;
+            Text = text;
+            libaryBooks.Add(this);
+        }
+
+        public static bool BookExists(int id)
+        {
+            try
+            {
+                GetBookById(id);
+                return true;
+            }
+            catch(KeyNotFoundException)
+            {
+                return false;
+            }
+        }
+        public static Book GetBookById(int id)
+        {
+            foreach(Book libaryBook in LibaryBooks)
+            {
+                if (libaryBook.Id == id)
+                    return libaryBook;
+            }
+            throw new KeyNotFoundException("no book with id: " + id.ToString() + " found.");
+        }
+    }
+}
