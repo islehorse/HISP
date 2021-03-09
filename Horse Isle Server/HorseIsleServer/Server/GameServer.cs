13435cf37fc90cf6240334477f439e85ac735224
SilicaAndPina <earsyum@gmail.com>
2021-03-10 01:29:57 +1300

    add some real time riddles text

--- Horse Isle Server/HorseIsleServer/Server/GameServer.cs
+++ Horse Isle Server/HorseIsleServer/Server/GameServer.cs
@@ -190,6 +190,12 @@ namespace HISP.Server
                 }
             }
 
+            if (RandomNumberGenerator.Next(0, 100) == 59) // Real Time Riddle
+            {
+
+            }
+
+
             Database.IncPlayerTirednessForOfflineUsers();
 
             // Offline player handling w sql magic...
