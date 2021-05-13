3e51b27ac83a91358ea60223afc608e0ba255d25
Li <li@silica.codes>
2021-05-13 12:00:54 +1200

    Make signup page better

---
+++ WebInterface/master-site/web/footer.php
@@ -0,0 +1,23 @@
+<TABLE BORDER=0 CELLPADDING=0 CELLSPACING=0 WIDTH=100%>
+<TR>
+<TD><IMG SRC=/web/hoilgui10.gif></TD>
+<TD WIDTH=100% BACKGROUND=/web/hoilgui11.gif></TD>
+<TD><IMG SRC=/web/hoilgui12.gif></TD>
+</TR></TABLE>
+<CENTER><B>
+[ <A HREF=//master.horseisle.com/beginnerguide/>New Player Guide</A> ]<BR>
+[ <A HREF=/web/rules.php>Rules</A> ]
+[ <A HREF=/web/termsandconditions.php>Terms and Conditions</A> ]
+[ <A HREF=/web/privacypolicy.php>Privacy Policy</A> ]</B><BR>
+[ <A HREF=/web/expectedbehavior.php>Expected Behavior</A> ]
+[ <A HREF=/web/contactus.php>Contact Us</A> ] 
+[ <A HREF=/web/credits.php>Credits</A> ]<BR>
+<FONT FACE=Verdana,Arial SIZE=-2>Copyright &copy; 2021 Horse Isle</FONT>
+
+<!-- Google Analytics -->
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-1805076-1";
+urchinTracker();
+</script>
