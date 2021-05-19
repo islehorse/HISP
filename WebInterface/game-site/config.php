197dc53270fcc76d92320242966ae1476b2eebf8
SilicaAndPina <earsyum@gmail.com>
2021-05-19 12:52:50 +1200

    Update comment

--- WebInterface/game-site/config.php
+++ WebInterface/game-site/config.php
@@ -7,9 +7,11 @@ $dbuser = 'root';
 $dbpass = 'test123';
 $dbhost = '127.0.0.1';
 
+# == hmac_secret ==
 # Used for master-site to communicate with game-sites,
 # Should be set to the same value on all game sites and the master site.
-# NOTE: if someone knows this secret they can create accounts with ADMIN privledges!
+# NOTE: if someone knows this secret they can login to ANYONES account
+# Ideally, this would be a random string of numbers, letters and symbols like 20 characters long T-T
 $hmac_secret = "!!NOTSET!!"; 
 $master_site = "http://server.islehorse.com";
 # Password for /web/admin
