47906b52ab3c675df36663d4095249ec22f55cf8
Li <li@silica.codes>
2021-06-27 18:20:51 +1200

    add new config

--- WebInterface/game-site/config.php
+++ WebInterface/game-site/config.php
@@ -9,6 +9,8 @@ $dbhost = '127.0.0.1';
 
 $all_users_subbed = false;
 $server_id = "beta";
+$pp_uri = '/web/ppemu.php'; # paypal emulator, original is https://www.paypal.com/cgi-bin/webscr
+$EXHANGE_RATE = 100000; # How much 1 USD is worth as HI Money
 
 # == hmac_secret ==
 # Used for master-site to communicate with game-sites,
@@ -16,7 +18,7 @@ $server_id = "beta";
 # NOTE: if someone knows this secret they can login to ANYONES account
 # Ideally, this would be a random string of numbers, letters and symbols like 20 characters long T-T
 $hmac_secret = "!!NOTSET!!";
-$master_site = "//localhost";
+$master_site = "//server.islehorse.com";
 # Password for /web/admin
 $admin_portal_password = "!!NOTSET!!";
 ?>
