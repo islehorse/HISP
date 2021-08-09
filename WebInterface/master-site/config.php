0a533e51845e2c72716fec6bf4de781f7c523b76
Li <li@silica.codes>
2021-08-09 19:01:21 +1200

    Update config.php

--- WebInterface/master-site/config.php
+++ WebInterface/master-site/config.php
@@ -4,6 +4,11 @@ $dbuser = 'root';
 $dbpass = 'test123';
 $dbhost = '127.0.0.1';
 
+$pp_uri = '[GAMESITE]/web/ppemu.php'; # location of paypal emulator on game-servers
+# [GAMESITE] is replaced with the URL for the game-site, as specified in servers.php
+# original is https://www.paypal.com/cgi-bin/webscr which obviously wont do
+# Dont set it to that though, as the paypalgateway.php is not implemented.
+
 #should be same as all game-site's
 $hmac_secret = '!!NOTSET!!';
 ?>
