413e961d453813bce66b155cbb358338b205a4df
Bluzume <39113159+KuromeSan@users.noreply.github.com>
2021-10-24 01:02:06 -0400

    manually delete servers.php

--- WebInterface/master-site/servers.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$server_list = [
-	['id' => 'game', 'icon' => 'beta.gif', 'database' => 'beta', 'site' => 'http://game.islehorse.com', 'desc' => ' '],
-];
-
-// black: 1/3 interest rate,  final new server as of July 2012 for those who want to start classic horseisle anew!
-// white: Subscriber only server.<br>You must subscribe to this server to play.<BR>If subscription runs out, you will no longer be able to play on it.
-?>
