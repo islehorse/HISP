a6971306a487d527c55cbf2d97e8deed751d352e
SilicaAndPina <earsyum@gmail.com>
2022-04-15 00:32:55 +1200

    Fix resources

--- HorseIsleServer/HISPd/Properties/Resources.Designer.cs
+++ HorseIsleServer/HISPd/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace HISP.Properties {
+namespace HISP.Cli.Properties {
     using System;
     
     
