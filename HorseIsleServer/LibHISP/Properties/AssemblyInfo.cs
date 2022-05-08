05c67eefbd2627f04fb8de2b3f1dad86a6193422
Li <li@silica.codes>
2022-05-08 12:51:39 +1200

    Update made automatically due to pressing build

--- HorseIsleServer/LibHISP/Properties/AssemblyInfo.cs
+++ HorseIsleServer/LibHISP/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HISP")]
+[assembly: AssemblyTitle("LibHISP")]
 [assembly: AssemblyDescription("Server Emulator for \"Horse Isle\"")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SilicaAndPina")]
+[assembly: AssemblyCompany("Li")]
 [assembly: AssemblyProduct("HISP")]
 [assembly: AssemblyCopyright("Public Domain © 2022")]
 [assembly: AssemblyTrademark("")]
@@ -30,6 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.3.0.0")]
 [assembly: AssemblyFileVersion("1.3.0.0")]
