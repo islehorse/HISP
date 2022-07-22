b83960d872d056d645fd034145e5f85ffe76712c
Li <li@silica.codes>
2022-07-22 18:57:53 +1200

    Finish MPN00Bs

---
+++ HorseIsleServer/MPN00BS/HispViewModel.cs
@@ -0,0 +1,106 @@
+﻿using HISP.Server;
+using MiniMvvm;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace MPN00BS.ViewModels
+{
+    public class HispViewModel : ViewModelBase
+    {
+        public HispViewModel()
+        {
+            ServerStarter.ReadServerProperties();
+            swearFilterHeader = (ConfigReader.EnableSwearFilter ? "Disable" : "Enable") + " Swear Filter";
+
+            createAccountCommand = MiniCommand.Create(() =>
+            {
+                if (!ServerStarter.HasServerStarted)
+                {
+                    MessageBox.Show(null, "There is no Horse Isle Server running yet.", "Server not Started.", MessageBox.MessageBoxButtons.Ok);
+                    return;
+                }
+                new RegisterWindow().Show();
+            });
+
+            resetPasswordCommand = MiniCommand.Create(() =>
+            {
+                if (!ServerStarter.HasServerStarted)
+                {
+                    MessageBox.Show(null, "There is no Horse Isle Server running yet.", "Server not Started.", MessageBox.MessageBoxButtons.Ok);
+                    return;
+                }
+
+                new ResetWindow().Show();
+            });
+
+            editServerPropertiesCommand = MiniCommand.Create(() =>
+            {
+                if (!ServerStarter.HasServerStarted)
+                {
+                    MessageBox.Show(null, "There is no Horse Isle Server running yet.", "Server not Started.", MessageBox.MessageBoxButtons.Ok);
+                    return;
+                }
+
+                Process p = new Process();
+                p.StartInfo.FileName = Path.Combine(ServerStarter.BaseDir, "server.properties");
+                p.StartInfo.UseShellExecute = true;
+                p.Start();
+
+            });
+
+            openServerFolderCommand = MiniCommand.Create(() =>
+            {
+                if (!ServerStarter.HasServerStarted)
+                {
+                    MessageBox.Show(null, "There is no Horse Isle Server running yet.", "Server not Started.", MessageBox.MessageBoxButtons.Ok);
+                    return;
+                }
+
+                Process p = new Process();
+                p.StartInfo.FileName = ServerStarter.BaseDir;
+                p.StartInfo.UseShellExecute = true;
+                p.Start();
+            });
+
+            shutdownServerCommand = MiniCommand.Create(() =>
+            {
+                if (!ServerStarter.HasServerStarted)
+                {
+                    MessageBox.Show(null, "There is no Horse Isle Server running yet.", "Server not Started.", MessageBox.MessageBoxButtons.Ok);
+                    return;
+                }
+
+                GameServer.ShutdownServer();
+            });
+
+
+            toggleSwearFilter = MiniCommand.Create(() =>
+            {
+                if (!ServerStarter.HasServerStarted)
+                {
+                    MessageBox.Show(null, "There is no Horse Isle Server running yet.", "Server not Started.", MessageBox.MessageBoxButtons.Ok);
+                    return;
+                }
+
+                bool enab = !ConfigReader.EnableSwearFilter;
+                ServerStarter.ModifyConfig("enable_word_filter", enab.ToString().ToLowerInvariant());
+                ConfigReader.EnableSwearFilter = enab;
+                swearFilterHeader = (ConfigReader.EnableSwearFilter ? "Disable" : "Enable") + " Swear Filter";
+            });
+            
+        }
+
+ 
+
+
+        public String swearFilterHeader { get; set; }
+        public MiniCommand shutdownServerCommand { get; }
+        public MiniCommand createAccountCommand { get; }
+        public MiniCommand editServerPropertiesCommand { get; }
+        public MiniCommand openServerFolderCommand { get; }
+        public MiniCommand toggleSwearFilter { get; }
+        public MiniCommand resetPasswordCommand { get; }
+
+    }
+}
---
+++ HorseIsleServer/MPN00BS/ResetWindow.axaml.cs
@@ -0,0 +1,147 @@
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using HISP.Security;
+using HISP.Server;
+using System;
+using System.Text.RegularExpressions;
+
+namespace MPN00BS
+{
+    public partial class ResetWindow : Window
+    {
+        public ResetWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void ValidateInput()
+        {
+            if (ValidateUsername() && ValidatePassword())
+                resetPasswordButton.IsEnabled = true;
+            else
+                resetPasswordButton.IsEnabled = false;
+        }
+
+        private bool ValidatePassword()
+        {
+            if (passwordBox == null)
+                return false;
+            if (passwordBox.Text == null)
+                return false;
+
+            int selStart = passwordBox.SelectionStart;
+            int selEnd = passwordBox.SelectionEnd;
+            passwordBox.Text = Regex.Replace(passwordBox.Text, "[^A-Za-z0-9]", "");
+            passwordBox.SelectionStart = selStart;
+            passwordBox.SelectionEnd = selEnd;
+
+            if (passwordBox.Text.Length < 6)
+            {
+                passwordValidationFailReason.Content = "- Password must be more than 6 characters.";
+                return false;
+            }
+
+            if (passwordBox.Text.Length >= 16)
+            {
+                passwordValidationFailReason.Content = "- Password must be less than 16 characters.";
+                return false;
+            }
+
+            passwordValidationFailReason.Content = "";
+            return true;
+        }
+
+        private bool ValidateUsername()
+        {
+            if (usernameBox == null)
+                return false;
+            if (usernameBox.Text == null)
+                return false;
+            int selStart = usernameBox.SelectionStart;
+            int selEnd = usernameBox.SelectionEnd;
+            usernameBox.Text = Regex.Replace(usernameBox.Text, "[^A-Za-z]", "");
+            usernameBox.SelectionStart = selStart;
+            usernameBox.SelectionEnd = selEnd;
+
+            if (usernameBox.Text.Length < 3)
+            {
+                usernameValidationFailReason.Content = "- Username must be more than 3 characters.";
+                return false;
+            }
+
+            if (usernameBox.Text.Length >= 16)
+            {
+                usernameValidationFailReason.Content = "- Username must be less than 16 characters.";
+                return false;
+            }
+
+            if (Regex.IsMatch(usernameBox.Text, "[A-Z]{2,}"))
+            {
+                usernameValidationFailReason.Content = "- Username have the first letter of each word capitalized.";
+                return false;
+            }
+
+            if (usernameBox.Text.ToUpper()[0] != usernameBox.Text[0])
+            {
+                usernameValidationFailReason.Content = "- Username have the first letter of each word capitalized.";
+                return false;
+            }
+
+            if (!Database.CheckUserExist(usernameBox.Text))
+            {
+                usernameValidationFailReason.Content = "- Username not found.";
+                return false;
+            }
+
+            usernameValidationFailReason.Content = "";
+            return true;
+        }
+
+        private void usernameChanged(object sender, KeyEventArgs e)
+        {
+            if (usernameBox == null)
+                return;
+
+            ValidateInput();
+        }
+        private void passwordChanged(object sender, KeyEventArgs e)
+        {
+            if (passwordBox == null)
+                return;
+
+            ValidateInput();
+
+        }
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+            resetPasswordButton = this.FindControl<Button>("resetPasswordButton");
+
+            usernameValidationFailReason = this.FindControl<Label>("usernameValidationFailReason");
+            passwordValidationFailReason = this.FindControl<Label>("passwordValidationFailReason");
+
+            usernameBox = this.FindControl<TextBox>("usernameBox");
+            passwordBox = this.FindControl<TextBox>("passwordBox");
+        }
+
+        private void ResetPassword(object sender, RoutedEventArgs e)
+        {
+            // Get salt
+            byte[] salt = Database.GetPasswordSalt(usernameBox.Text);
+
+            // Hash password
+            string hashsalt = BitConverter.ToString(Authentication.HashAndSalt(passwordBox.Text, salt)).Replace("-", "");
+
+            Database.SetPasswordHash(usernameBox.Text, hashsalt);
+
+            this.Close();
+        }
+
+    }
+
+}
