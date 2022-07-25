d60e76354aae04bcbe8453b669d0f1d14ada7df1
Li <li@silica.codes>
2022-07-25 21:45:21 +1200

    Update MPN00BS

--- HorseIsleServer/MPN00BS/Program.cs
+++ HorseIsleServer/MPN00BS/Program.cs
@@ -2,11 +2,15 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using System;
+using System.Runtime.InteropServices;
 
 namespace MPN00BS
 {
     internal class Program
     {
+
+
+
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
