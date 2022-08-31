72882c3c3c38aa9e831302192df736600f421298
Li <li@silica.codes>
2022-08-31 20:38:08 +1200

    Make RandomId Thread Safe

--- HorseIsleServer/LibHISP/Security/RandomID.cs
+++ HorseIsleServer/LibHISP/Security/RandomID.cs
@@ -1,19 +1,25 @@
-﻿namespace HISP.Security
+﻿using System.Threading;
+
+namespace HISP.Security
 {
     public class RandomID
     {
+        private static Mutex rndmIdMutex = new Mutex();
         private static int prevId = 0;
         public static int NextRandomId(int randomId=-1)
         {
             int rndmId = 0;
-
+            rndmIdMutex.WaitOne();
+            
             if (randomId == -1)
                 rndmId = prevId+1;
             else
                 rndmId = randomId;
-
+            
             if (rndmId >= prevId)
                 prevId = rndmId;
+
+            rndmIdMutex.ReleaseMutex();
 
             return rndmId;
         }
