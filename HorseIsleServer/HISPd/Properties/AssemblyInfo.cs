892376a83502989709b38d02c46785b68d2114ad
Li <li@silica.codes>
2022-09-01 16:20:11 +1200

    Add back HISPd AssemblyInfo.cs

---
+++ HorseIsleServer/HISPd/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HISPd")]
+[assembly: AssemblyDescription("Server Emulator for \"Horse Isle\"")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Li")]
+[assembly: AssemblyProduct("HISP")]
+[assembly: AssemblyCopyright("Public Domain � 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("57264c6b-1461-41d6-9304-3890cf6c8390")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.7.133.0")]
+[assembly: AssemblyFileVersion("1.7.133.0")]
