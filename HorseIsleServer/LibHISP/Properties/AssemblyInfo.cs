7fcaf412df0f4d1bf47c599f3e89a3631f1cd540
Li <li@silica.codes>
2022-11-18 19:51:25 +1300

    Fix build

--- HorseIsleServer/LibHISP/Properties/AssemblyInfo.cs
+++ HorseIsleServer/LibHISP/Properties/AssemblyInfo.cs
@@ -30,8 +30,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.8.45.0")]
-[assembly: AssemblyFileVersion("1.8.45.0")]
+[assembly: AssemblyVersion("1.8.46.0")]
+[assembly: AssemblyFileVersion("1.8.46.0")]
 
 
 
